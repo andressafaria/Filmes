b6e2e9f24a7f95e266ef4ae4de07e756b91338e7
Andressa <andressa_faria18@hotmail.com>
2015-06-20 18:37:18 -0300

    ação nova categoria
    pagina php que executa a ação para cadastrar uma nova categoria

---
+++ admin/NovaCategoriaAcao.php
@@ -0,0 +1,23 @@
+ <?php include "../conn.php";   
+
+//aqui estou recuperando os dados do formulário vindos da página NovaCategoia.php
+ 
+  $nome = $_POST['nome'];
+	  
+//Verifico se a ação for diferente de vazio, ou seja, se clicar em cadastrar insiro no banco	  
+  	if(!empty($_POST['metodo'])){
+	  
+   		mysql_query("insert into categorias(categoria) values ('".$nome."')");
+    		
+// Em seguida apresento a mensagem de sucesso, caso tenha sido cadastrado		
+		echo'Categoria cadastrada com sucesso!';
+		
+		
+		
+		
+		
+		//Ver como redirecionar a página para a lst_categoria.php... não está funcionando
+			 
+	
+	}
+?>
