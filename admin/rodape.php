c394e68adb20a7401fe15a31584d791aaa3ecc4d
Andressa <andressa_faria18@hotmail.com>
2015-06-20 16:47:10 -0300

    rodape
    pagina que contém o rodapé que será adicionado as páginas

---
+++ admin/rodape.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>rodape</title>
+</head>
+
+<body>
+	<div id="footer">
+			Filmes e Cia todos os direitos reservados 2015  |
+			<a href="institucional.php">Institucional	|</a>
+	</div>
+
+</body>
+</html>
+
+
+<!--pensar no que mais adicionar ao rodapé; se der tempo colocar links para redes sociais-->
