31cb83eb88d81acfd29b5e90c904348d59e9da92
Andressa <andressa_faria18@hotmail.com>
2015-06-20 18:06:16 -0300

    Novo usuario açao
    pagina que executa a ação para adicionar um novo usuario.

---
+++ admin/NovoUsuarioAcao.php
@@ -0,0 +1,25 @@
+<?php
+// incluindo a conexão com o banco de dados
+require "../conn.php"; 
+
+// recuperando as variáveis do formulário via POST
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$entrar = $_POST['entrar'];
+$senha=$_POST['senha'];
+	  
+//Verificando se o formuçario está vazio... Se não estiver executa a query para salvar os dados no banco	  
+if(!empty($_POST['metodo'])){
+	  
+	  	mysql_query("insert into administracao(nome,email,login,senha) values ('".$nome."','".$email."','".$entrar."','".$senha."')");
+		$html= 'Administrador cadastrado com sucesso!';
+		echo $html;
+		
+		
+		
+	    
+// faltando redirecionar para a página anterior... está dando um erro... verificar.    		
+
+}
+	
+?>
