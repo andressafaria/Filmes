342be7405f8d41e9e1a46606a212173e0cdf9481
Andressa <andressa_faria18@hotmail.com>
2015-06-20 16:41:19 -0300

    Menu
    menu que será localizado do lado direito da página

---
+++ admin/menu.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Movies.com</title>
+</head>
+
+<body>
+<!-- inicio da div right-->			 
+<div class="right">
+	<!-- inicio da div featbox rounded-->
+    <div class="featbox rounded">
+		<!-- inicio da div featlist-->		 
+        <div class="featlist ">
+		  <!-- inicio da classe fblock-->
+			<div class="fblock">		
+				<h3 class="sidetitl rounded">Cadastrar</h3>
+          <!-- fim da classe fblock-->
+    		</div>
+          <!-- inicio da classe fblock-->
+			<div class="fblock">		
+	    		<a href="lst_categoria.php">-Categorias</a>
+          <!-- fim da classe fblock-->
+			</div>
+			<!--<div class="fblock">
+	    		<a href="lst_subcategoria.php">-SubCategorias</a><br />
+			</div>-->
+          <!-- inicio da classe fblock-->
+			<div class="fblock">
+	    		<a href="lst_filmes.php">-Filmes</a>
+            <!-- fim da classe fblock-->
+			</div>
+							
+<!-- fim da div fblock-->  		  
+		</div>
+<!-- fim da div featlist-->	
+	  </div>
+<!-- fim da div featbox rounded-->
+</div>
+<!-- fim da div right-->		 
+
+
+
+
+<!-- Este menu é o que ficará localizando do lado direito da página com algumas funções para cadastrar filmes e categorias--> 
+</body>
+</html>
