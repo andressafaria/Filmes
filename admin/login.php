1294c23138992c01a33b49e86a280dd8e9efd3e4
Andressa <andressa_faria18@hotmail.com>
2015-06-20 14:34:39 -0300

    Formulário de login
    formulário para o login

---
+++ admin/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Movies.com</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+<body>		
+	<div id="barra"></div>
+		<div class="logo">
+				<h1>Movies.com</h1>
+		</div>
+			
+<div class="gravar">
+		<div class="inputgravar">                                                             
+			  <table cellpadding="0" cellspacing="0">
+			    	<form method="post" action="login_vai.php">
+			      		<tr>
+			       			 <td colspan="3"><div align="center"><img src="images/lock.png" /><strong> &Aacute;rea restrita </strong></div></td>
+     				   </tr>
+			     	   <tr>
+			      			 <td  rowspan="3"><img src="images/login.png" align="top" width="100" height="120" /></td>
+    					     <td >Login:</td>
+      						<td ><input type="text" name="login"/></td>
+    					</tr>
+			      		<tr>
+			       			 <td>Senha:</td>
+     						 <td><input type="password" name="senha" /></td>
+    			 	   </tr>
+			      	   <tr>
+			      		    <td colspan="2"><input type="submit" value="Enviar" /></td>
+      				   </tr>
+		           </form>
+              </table>
+			  			 
+		</div>
+</div>
+
+</body>
+</html>
